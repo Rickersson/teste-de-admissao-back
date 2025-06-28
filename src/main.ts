606eadc4b38723c2fdb771b99d02f4e10642ec7b
Rickersson <henrique.senacnig@gmail.com>
2025-06-27 21:42:04 -0300

    feat: autorização de cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as cors from 'cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  
+  const corsOptions: cors.CorsOptions = {
+    origin: 'http://localhost:4200', 
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+    allowedHeaders: 'Content-Type, Accept, Authorization',
+  };
+
+  app.use(cors(corsOptions)); 
+
+  await app.listen(3000);
 }
 bootstrap();
